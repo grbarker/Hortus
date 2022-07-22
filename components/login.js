3ef646477f8e5d5925240777eb75f5385f417f88
Greg Barker <grbarker04@gmail.com>
2022-07-22 12:38:12 -0700

    Fix require cycle made by login component and loginForm component. Moved all login logic back to teh login component.

--- components/login.js
+++ components/login.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ScrollView, Text, View, StyleSheet } from 'react-native';
 import { AsyncStorage } from "react-native";
-import { login, submitLogin, loginSubmit, getTokenSuccess, getTokenFailure } from '../actions/auth';
+import * as SecureStore from 'expo-secure-store';
+import { login, loginFail, submitLogin, loginSubmit, getTokenSuccess, getTokenFailure } from '../actions/auth';
 import {
   white, my_green, green, gray4, red, purple, orange, blue, my_blue,
   lightPurp, black, pink
@@ -14,6 +15,66 @@ const api = "http://45.79.227.26/catalog/mobilelogin/JSON"
 
 class Login extends Component {
 
+  async saveUserInfo(uname, pword) {
+    await SecureStore.setItemAsync('username', uname);
+    await SecureStore.setItemAsync('password', pword);
+  }
+
+  async userLogin(values) {
+      const { dispatch, username, password } = this.props
+      let uname = username ? username : values.username
+      let pword = password ? password : values.password
+      console.log('These are the input values for Log In:    ', values);
+      try {
+        let response = await fetch(
+          `http://${uname}:${pword}@45.79.227.26/api/tokens`, {
+            method: 'POST',
+          }
+        );
+        console.log(response);
+        let responseJSON = await response.json();
+        console.log('This is the login response:    ', responseJSON);
+        if (responseJSON.error) {
+          dispatch(loginFail(responseJSON.error, responseJSON.message));
+        } else {
+          let token = responseJSON.token;
+          dispatch(login(values.username, values.password, token));
+          this.saveUserInfo(values.username, values.password)
+        }
+      } catch (error) {
+        console.error('react native form error:   ', error);
+      }
+  }
+  async userSignup (values) {
+      const { dispatch } = this.props
+      console.log('These are the input values for Sign Up:    ', values);
+
+      try {
+        let response = await fetch(
+          `http://45.79.227.26/api/users`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              "username": `${values.username}`,
+              "email": `${values.email}`,
+              "password": `${values.password}`,
+            })
+          }
+        );
+        let responseJSON = await response.json();
+        console.log("This is the Sign Up response:____________", responseJSON);
+        this.userLogin(values)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+  onSubmit = (values) => {
+    this.props.loginScreen ? this.userLogin(values) : this.userSignup(values)
+
+  };
 
 
   render () {
@@ -22,7 +83,7 @@ class Login extends Component {
       return (
           <ScrollView style={{flex: 1, padding: 20}}>
             <Text style={{flex: 1, fontSize: 27, color: my_green}}>{loginScreen ? "Log In" : "Sign Up"}</Text>
-            <LoginForm  loginScreen={loginScreen ? true : false} />
+            <LoginForm  onSubmit={this.onSubmit} loginScreen={loginScreen ? true : false} />
             {error
               ? <Text style={styles.errorText}>{message}</Text>
               : null
--- components/loginForm.js
+++ components/loginForm.js
@@ -2,83 +2,21 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 import { ScrollView, View, Text, TextInput,TouchableOpacity, StyleSheet, Button, Platform } from 'react-native'
 import TextButton from './TextButton'
-import * as SecureStore from 'expo-secure-store';
 import 'moment-timezone';
 import { connect } from 'react-redux'
 import {
   white, black, gray, purple, green, blue, my_green, my_blue, pink, lightPurp,
   red, orange
 } from '../utils/colors'
-import { login, loginFail } from '../actions/auth';
 import renderField from './renderField'
 import renderPasswordField from './renderPasswordField'
 import axios from 'axios';
-import { loginSubmit } from './login'
 
 
 
 class LoginForm extends Component {
 
-  async saveUserInfo(uname, pword) {
-    await SecureStore.setItemAsync('username', uname);
-    await SecureStore.setItemAsync('password', pword);
-  }
 
-  async userLogin(values) {
-      const { dispatch, username, password } = this.props
-      let uname = username ? username : values.username
-      let pword = password ? password : values.password
-      console.log('These are the input values for Log In:    ', values);
-      try {
-        let response = await fetch(
-          `http://${uname}:${pword}@45.79.227.26/api/tokens`, {
-            method: 'POST',
-          }
-        );
-        console.log(response);
-        let responseJSON = await response.json();
-        console.log('This is the login response:    ', responseJSON);
-        if (responseJSON.error) {
-          dispatch(loginFail(responseJSON.error, responseJSON.message));
-        } else {
-          let token = responseJSON.token;
-          dispatch(login(values.username, values.password, token));
-          this.saveUserInfo(values.username, values.password)
-        }
-      } catch (error) {
-        console.error('react native form error:   ', error);
-      }
-  }
-  async userSignup (values) {
-      const { dispatch } = this.props
-      console.log('These are the input values for Sign Up:    ', values);
-
-      try {
-        let response = await fetch(
-          `http://45.79.227.26/api/users`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              "username": `${values.username}`,
-              "email": `${values.email}`,
-              "password": `${values.password}`,
-            })
-          }
-        );
-        let responseJSON = await response.json();
-        console.log("This is the Sign Up response:____________", responseJSON);
-        this.userLogin(values)
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-  onSubmit = (values) => {
-    this.props.loginScreen ? this.userLogin(values) : this.userSignup(values)
-
-  };
 
   render() {
     const { error, handleSubmit, submitting, reset, pristine, data, invalid,
@@ -87,7 +25,7 @@ class LoginForm extends Component {
 
 
     return (
-      <ScrollView onSubmit={handleSubmit(this.onSubmit)}>
+      <ScrollView onSubmit={handleSubmit}>
         <Field
           name="username"
           type="text"
@@ -117,7 +55,7 @@ class LoginForm extends Component {
         />
         {error && <Text style={styles.errorText}>{error}</Text>}
         <View>
-          <Button title='Submit' type="submit" disabled={pristine || submitting || invalid} onPress={handleSubmit(this.onSubmit)}>
+          <Button title='Submit' type="submit" disabled={pristine || submitting || invalid} onPress={handleSubmit}>
             Submit
           </Button>
           <Button title='Cancel' type="button" disabled={pristine || submitting} onPress={reset}>
